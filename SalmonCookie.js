a1a9b1686e5b11d9cfc6bab9a80eff29bf10e6d4
ixnp <enprocopios@gmail.com>
2017-02-18 14:41:30 -0800

    working up to time of day, fixed time of day property in constructor

--- SalmonCookie.js
+++ SalmonCookie.js
@@ -1,43 +1,60 @@
 // All Stores capacity is 60
 //pp= pike place location  //
 // table with location, time, cookies sold that hour and total per day
+
 function salmonCookieShop(name, minimum, maximum, averageCookiesPerCustomer){
+  console.log('~~~~');
   this.name= name;
+    console.log('name',name);
   this.minimum = minimum;
+    console.log('minimum', minimum);
   this.maximum = maximum;
+    console.log('maximim', maximum);
   this.averageCookiesPerCustomer = averageCookiesPerCustomer;
+    console.log('averageCookiesPerCustomer',averageCookiesPerCustomer);
+  timeOfDay = ['10:00','11:00','12:00','1:00','2:00','3:00','4:00','5:00'];
+    console.log('timeOfDay',timeOfDay);
   this.salePerHour= [];
+  console.log()
 //Random number of customers//
-  this.customerPerHour = function(){
-    return (Math.round(Math.random()*(this.maximum - this.minimum + 1 ))) +  this.minimum;
+  this.customerPerHour = function(maximum, minimum){
+console.log('maximum, minimum', maximum, minimum);
+    return Math.random()*(this.maximum - this.minimum + 1 ) +  this.minimum;
   };
-};
+
 
 //cookies per hour//
 
 this.cookierPerHour = function (){
-  for(i=0; i<6; i++)
-    hour = Math.round(this.customerPerHour()*this.averageCookiesPerCustomer);
-    this.salePerHour.push(hour);
-    // push adds items to the end of the array and returns the new length//
+  for(var i=0; i < this.timeOfDay.length; i++){
+    var random = this.averageCookiesPerCustomer * this.customerPerHour();
+    this.salePerHour.push(random);
+  };
+    console.log(cookierPerHour);
 };
-// Justins way: I'm not sure I really understand what you were doing here//
-salmonCookieShop.prototype.render = function(){
-var trElement = document.createElement('tr');
 
-var tdElement = document.createElement('td');
+this.renderCookiesPerHour = function() {
+  this.cookiesPurchased();
+  var table = document.getElementById('shopTable');
+  var tr = document.createElement('tr');
+  table.appendChild(tr);
+  var tdNames =document.createElement('td');
+  tdNames.appendChild(document.createTextNode(this.name));
+  tr.appendChild(tdNames);
 
-  tdElement.innerHTML = this.total;
-  tdElement.innerHTML = this.name;
-  trElement.appendChild(tdElement);
-
-
-
-  for(var i=0; i<6; i++){
-    td = document.createElement('td');
-    td.innerHTML = the.salePerHour[i];
-    trElement.appendChild(td);
+  for (var i = 0; i< this.timeOfDay.length; i++){
+    var td = document.createElement('td');
+    td.appendChild(document.createTextNode(Math.Floor(this.cookiesPurchasedArray[i]) + 'cookies.'));
+  };
 }
+}
+
+    // push adds items to the end of the array and returns the new length//
+
+// Justins way: I'm not sure I really understand what you were doing here//
+// this.renderCookies = function(){
+//   this.cookiesPerHour();
+
 //this is how I was trying to do it on my own//
 // this.renderAsRow = function(){
 //   var shopTable = document.getElementById('shopTable');
@@ -65,7 +82,7 @@ var PikePlace = new salmonCookieShop ('Pike Place',77,88, 5.2);
 var SeaTac =  new salmonCookieShop ('SeaTac', 6, 24, 1.2);
 var SouthCenter = new salmonCookieShop ('SouthCenter', 11, 38, 1.9);
 var BellevueSquare = new salmonCookieShop ('Bellevue Square', 20, 48, 3.3);
-}
+
 // PikePlace.render();
 // SeaTac.render();
 // SouthCenter.render();
