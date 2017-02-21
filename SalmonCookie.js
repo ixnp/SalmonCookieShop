0c35f2466325cf66eb0b69d5da1eca59bffa10b4
ixnp <enprocopios@gmail.com>
2017-02-20 18:16:04 -0800

    Table working!!!!!

--- SalmonCookie.js
+++ SalmonCookie.js
@@ -2,28 +2,34 @@
 //pp= pike place location  //
 // table with location, time, cookies sold that hour and total per day
 
+// var allItems = [];
+var PikePlace = new salmonCookieShop ('Pike Place',77,88, 5.2);
+var SeaTac =  new salmonCookieShop ('SeaTac', 6, 24, 1.2);
+var SouthCenter = new salmonCookieShop ('SouthCenter', 11, 38, 1.9);
+var BellevueSquare = new salmonCookieShop ('Bellevue Square', 20, 48, 3.3);
+
+var table = document.getElementById('shopTable');
+var tbody = document.getElementById('shopTable2');
+
 function salmonCookieShop(name, minimum, maximum, averageCookiesPerCustomer){
   console.log('~~~~');
   this.name= name;
-    console.log('name',name);
+  console.log('name',name);
   this.minimum = minimum;
-    console.log('minimum', minimum);
+  console.log('minimum', minimum);
   this.maximum = maximum;
-    console.log('maximum', maximum);
+  console.log('maximum', maximum);
   this.averageCookiesPerCustomer = averageCookiesPerCustomer;
-    console.log('averageCookiesPerCustomer',averageCookiesPerCustomer);
+  console.log('averageCookiesPerCustomer',averageCookiesPerCustomer);
   timeOfDay = ['10:00','11:00','12:00','1:00','2:00','3:00','4:00','5:00'];
-    console.log('timeOfDay',timeOfDay);
+  console.log('timeOfDay',timeOfDay);
 
-    // this.cookiesPerHour = [];
-      // console.log('cookiesPerHour', cookiesPerHour);
-  // this.customersPerHour= customersPerHour();
-  //
+  //sale of cookies per customer//
   this.salePerHour= [];
 
 
 
-//Random number of customers//
+  //Random number of customers//
   this.customersPerHour = function(){
     console.log('customersPerHour');
     console.log('maximum, minimum',this.maximum, this.minimum );
@@ -32,57 +38,95 @@ function salmonCookieShop(name, minimum, maximum, averageCookiesPerCustomer){
   console.log('random customer per hour',this.customersPerHour());
 
 
-//cookies per hour//
+  //cookies per hour//
 
-this.cookiesPerHour = function (){
-  console.log('TOD array length', timeOfDay.length);
-  console.log('random customer per hour before loop',this.customersPerHour());
-  for(var i=0; i < timeOfDay.length; i++){
-    console.log('random customer per hour after loop',this.customersPerHour());
-    var x = this.customersPerHour()
-    var random = Math.round( x * this.averageCookiesPerCustomer);
-    this.salePerHour.push(random);
-  };
-  console.log(this.salePerHour);
+  this.cookiesPerHour = function (){
+    // console.log('TOD array length', timeOfDay.length);
+    // console.log('random customer per hour before loop',this.customersPerHour());
+    for(var i=0; i < timeOfDay.length; i++){
+      // console.log('random customer per hour after loop',this.customersPerHour());
+      var x = this.customersPerHour();
+      var random = Math.round( x * this.averageCookiesPerCustomer);
+      this.salePerHour.push(random);
+    };
+
+    // console.log(this.salePerHour);
     // console.log(this.cookiesPerHour());
-};
-console.log('salePerHour', this.cookiesPerHour());
-
-this.renderCookiesPerHour = function() {
-  this.cookiesPurchased();
-  var table = document.getElementById('shopTable');
-  var tr = document.createElement('tr');
-  table.appendChild(tr);
-  var tdNames =document.createElement('td');
-  tdNames.appendChild(document.createTextNode(this.name));
-  tr.appendChild(tdNames);
-
-  for (var i = 0; i< this.timeOfDay.length; i++){
-    var td = document.createElement('td');
-    td.appendChild(document.createTextNode(Math.Floor(this.cookiesPurchasedArray[i]) + 'cookies.'));
   };
+  this.cookiesPerHour();
+  // console.log('salePerHour', this.cookiesPerHour());
+  this.makeItemRow = function(){
+    var table = document.getElementById('shopTable');
+    var row = document.createElement('tr');
+    table.appendChild(row);
+
+    var nameCell = document.createElement('td');
+    nameCell.textContent = this.name;
+    row.appendChild(nameCell);
+
+    for(var i=0; i< this.salePerHour.length; i++ ){
+      var aveCell = document.createElement('td');
+      aveCell.textContent = this.salePerHour[i];
+      row.appendChild(aveCell);
+    }
+  }
+  this.makeItemRow();
 }
-}
-function makeItemRow(obj) {
-  var row = document.createElement('tr');
-
-  var nameCell = document.createElement('td');
-  nameCell.textContent = obj.name;
-  row.appendChild(nameCell);
-
-  var priceCell = document.createElement('td');
-  priceCell.textContent = obj.timeOfDay;
-  row.appendChild(priceCell);
-
-  var taxCell = document.createElement('td');
-  taxCell.textContent = obj.averageCookiesPerCustomer;
-  row.appendChild(taxCell);
 
 
-  tbody.appendChild(row);
-}
 
-    // push adds items to the end of the array and returns the new length//
+// }
+// function makeAllItemRows() {
+//   for (var item of allItems) {
+//     makeItemRow(item);
+//   }
+// }
+// makeAllItemRows();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
+// function makeItemRow(obj) {
+//   var row = document.createElement('tr');
+//
+//   var nameCell = document.createElement('td');
+//   nameCell.textContent = obj.name;
+//   row.appendChild(nameCell);
+//
+//   var priceCell = document.createElement('td');
+//   priceCell.textContent = obj.timeOfDay;
+//   row.appendChild(priceCell);
+//
+//   var taxCell = document.createElement('td');
+//   taxCell.textContent = obj.averageCookiesPerCustomer;
+//   row.appendChild(taxCell);
+//
+//
+//   tbody.appendChild(row);
+// }
+
+// push adds items to the end of the array and returns the new length//
 
 // Justins way: I'm not sure I really understand what you were doing here//
 // this.renderCookies = function(){
@@ -111,13 +155,9 @@ function makeItemRow(obj) {
 //
 // // // shops///
 //
-var PikePlace = new salmonCookieShop ('Pike Place',77,88, 5.2);
-// var SeaTac =  new salmonCookieShop ('SeaTac', 6, 24, 1.2);
-// var SouthCenter = new salmonCookieShop ('SouthCenter', 11, 38, 1.9);
-// var BellevueSquare = new salmonCookieShop ('Bellevue Square', 20, 48, 3.3);
 
-PikePlace.render();
+
+// PikePlace.render();
 // SeaTac.render();
 // SouthCenter.render();
 // BellevueSquare.render();
-makeAllItemRows();
